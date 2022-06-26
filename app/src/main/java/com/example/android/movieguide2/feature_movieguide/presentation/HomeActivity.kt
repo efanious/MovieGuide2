4077b7a9b07cb7ee0d3adb1b83d8c076814f7014
Efana <efana.usua@wemabank.com>
2022-06-26 17:26:20 +0100

    Used hilt to wire trending movies with VM

--- app/src/main/java/com/example/android/movieguide2/feature_movieguide/presentation/HomeActivity.kt
+++ app/src/main/java/com/example/android/movieguide2/feature_movieguide/presentation/HomeActivity.kt
@@ -1,8 +1,11 @@
 package com.example.android.movieguide2.feature_movieguide.presentation
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import com.example.android.movieguide2.R
+import com.example.android.movieguide2.feature_movieguide.presentation.trending.TrendingActivity
 import dagger.hilt.android.AndroidEntryPoint
 
 
@@ -10,5 +13,14 @@ class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
+        val trendingBtn = findViewById<Button>(R.id.trending_button)
+        trendingBtn.setOnClickListener {
+            val intent = Intent(
+                this,
+                TrendingActivity::class.java
+            )
+            startActivity(intent)
+        }
     }
 }
