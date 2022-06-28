c1ed76bad470760cb4b6b971080d8db9d97a1f66
Efana <efana.usua@wemabank.com>
2022-06-28 12:41:21 +0100

    Narrowed challenge to using Hilt in ViewModels

--- app/src/main/java/com/example/android/movieguide2/feature_movieguide/presentation/trending/TrendingActivity.kt
+++ app/src/main/java/com/example/android/movieguide2/feature_movieguide/presentation/trending/TrendingActivity.kt
@@ -5,23 +5,24 @@ import android.os.Bundle
 import android.widget.Toast
 import androidx.lifecycle.ViewModelProvider
 import com.example.android.movieguide2.R
+import dagger.hilt.android.AndroidEntryPoint
 
-
-//@AndroidEntryPoint
+@AndroidEntryPoint
 class TrendingActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_trending)
 
-        val viewModel: TrendingViewModel =
-            ViewModelProvider(this)[TrendingViewModel::class.java]
-
-
-        viewModel.getTrendingMovies()
-
-        viewModel.response.observe(this) {
-            Toast.makeText(this@TrendingActivity, "${ it.results?.size ?:"Empty"  }", Toast.LENGTH_SHORT).show()
-        }
+        //val exampleViewModel: TrendingViewModel by viewModels()
+//        val viewModel: TrendingViewModel =
+//            ViewModelProvider(this)[TrendingViewModel::class.java]
+//
+//
+//        viewModel.getTrendingMovies()
+//
+//        viewModel.response.observe(this) {
+//            Toast.makeText(this@TrendingActivity, "${ it.results?.size ?:"Empty"  }", Toast.LENGTH_SHORT).show()
+//        }
 
     }
 }
